9602d00a559189a88f2f8c3269a9bfbcbaf17685
Filonzi Jacopo <st10357@iismarconipieralisi.it>
2023-11-13 18:07:38 +0100

    feature update to the watch command

--- commands/watch.js
+++ commands/watch.js
@@ -6,49 +6,69 @@ module.exports = {
     description: "Get the members with the role",
     execute: async (client, message, split) => {
 
-        if (!message.member.permissions.has("ADMINISTRATOR")) 
-            return; //administrator required
-
-            
-        if (split.length < 3) {
-            message.channel.send("Please specify a role to watch")
-            return
-        }
-
-        const role = message.mentions.roles.first();
-        
-        if (!role)
-            return message.channel.send("Please specify a role to watch")
-
-        //check if is a role of the guild
-        if (!message.guild.roles.cache.has(role.id)) {
-            message.channel.send("This role seems to be not from this guild");
-            return;
-        }
-
-    //#region SQL
-
         let _auth = {
             host: process.env.MYSQL_HOST,
             database: process.env.MYSQL_DATABASE,
             user: process.env.MYSQL_USER,
             password: process.env.MYSQL_PASSWORD
-          };
+        };
 
-        
-        const {results} = await mysql("SELECT * FROM cache WHERE role_id=?",[role.id], _auth)
+        //Continue only if an administrator called the command
+        if (!message.member.permissions.has("ADMINISTRATOR"))
+            return; //administrator required
+
+        //Get the role
+        const role = message.mentions.roles.first();
+
+        //If the user specify a role
+        if (role) {
+            //check if is a role of the guild
+            if (!message.guild.roles.cache.has(role.id)) {
+                message.channel.send("This role seems to be not from this guild");
+                return;
+            }
+
+            //Get the users with the role
+            const { results } = await mysql("SELECT * FROM cache WHERE role_id=?", [role.id], _auth)
+
+            //Map the query result
+            let users = results.map(user => user.user_id);
+
+            //Create a message base
+            let text = `Users with the role: <@&${role.id}>\n`
+
+            //Add every user to the message
+            for (const userId of users) {
+                text += `<@${userId}>\n`
+            }
+
+            //Send the message
+            message.channel.send(text)
+
+        } else { //Give all the monitored role
+                
+                //Get the roles
+                const { results } = await mysql("SELECT * FROM roles WHERE guild_id=?", [message.guild.id], _auth)
     
-        let users = results.map(user => user.user_id);
-
-        let text = `Users with the role: <@&${role.id}>\n`
-
-        for (const userId of users) {
-            text += `<@${userId}>\n`
+                //Map the query result
+                let roles = results.map(role => role.id);
+    
+                //Create a message base
+                let text = `Monitored roles:\n`
+    
+                //Add every role to the message
+                for (const roleId of roles) {
+                    let role = message.guild.roles.cache.get(roleId);
+                    if (role) {
+                        text += `<@&${roleId}> (${roleId})\n`
+                    }
+                }
+    
+                //Send the message
+                message.channel.send(text)
         }
-
-        message.channel.send(text)
 
     }
 }
 
-    
+
